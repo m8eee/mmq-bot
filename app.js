a02809d6fb646e948a6f45be9e55a886cb840ebc
m8eee <aldous0601@yahoo.com>
2020-11-18 20:45:13 +0800

    first

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.send("Hi!");
+    res.send("Hello!");
 });
 
 var verify_token = "changeMathForever"
